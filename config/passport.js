d557fd4aade18f1ac32ad4b48ccff7f3417062b3
pmhxhsj <minho7952@naver.com>
2022-02-23 18:53:42 +0900

    feat: google Oauth 임시 연동 - passport

---
+++ config/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const dotenv = require('dotenv');
+dotenv.config();
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: '/auth/google/callback',
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      console.log('profile: ', profile);
+      var user = profile;
+
+      done(null, user);
+    }
+  )
+);
+
+module.exports = passport;
---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('../config/passport.js');
+
+router.get('/login', function (req, res) {
+  res.render('auth/login');
+});
+
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/auth/login' }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
+
+module.exports = router;
