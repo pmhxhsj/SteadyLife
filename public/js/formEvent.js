bdea71b84ab8790d5f5f042315fb58ff7724ec09
pmhxhsj <minho7952@naver.com>
2022-02-25 03:34:47 +0900

    feat: form event - create클릭 시 form view - close클릭 시 form hidden

---
+++ public/js/formEvent.js
@@ -0,0 +1,28 @@
+const currentTime = () => {
+  const today = new Date();
+  const hours = String(today.getHours()).padStart(2, '0'); // 시
+  const minutes = String(today.getMinutes()).padStart(2, '0'); // 분
+
+  const startTime = document.querySelector('#todo-start-time-input');
+
+  startTime.value = hours + ':' + minutes;
+};
+
+const displayTodoCreateForm = () => {
+  document.querySelector('.create-list-btn').addEventListener('click', (e) => {
+    const target = document.querySelector('.to-do-create-container');
+
+    target.classList.remove('hidden');
+    target.classList.add('appear');
+  });
+
+  document
+    .querySelector('#form-close-button')
+    .addEventListener('click', (e) => {
+      const target = document.querySelector('.to-do-create-container');
+
+      target.classList.add('hidden');
+    });
+};
+
+export { currentTime, displayTodoCreateForm };
