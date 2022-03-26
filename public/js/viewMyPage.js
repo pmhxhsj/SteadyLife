99996ce88cbc117585325fc1f5717cbb0c201932
pmhxhsj <minho7952@naver.com>
2022-03-27 00:16:47 +0900

    feat: iphone media query 적용

--- public/js/viewMyPage.js
+++ public/js/viewMyPage.js
@@ -5,13 +5,11 @@ const displayMyPage = () => {
     const mypagebtn = document.querySelector('#myPage-button');
     const todobtn = document.querySelector('#todolist-button');
     const calendar = document.querySelector('.calendar');
-    const billboard = document.querySelector('#pieChart');
 
     todobtn.classList.remove('focus');
     mypagebtn.classList.add('focus');
 
     todo.classList.add('hidden');
-    billboard.classList.add('hidden');
 
     mypage.classList.remove('hidden');
     mypage.classList.add('appear');
@@ -28,15 +26,12 @@ const displayTodo = () => {
     const mypagebtn = document.querySelector('#myPage-button');
     const todobtn = document.querySelector('#todolist-button');
     const calendar = document.querySelector('.calendar');
-    const billboard = document.querySelector('#pieChart');
 
     todobtn.classList.add('focus');
     mypagebtn.classList.remove('focus');
 
     todo.classList.remove('hidden');
     todo.classList.add('appear');
-    billboard.classList.remove('hidden');
-    billboard.classList.add('appear');
     mypage.classList.add('hidden');
     calendar.classList.add('hidden');
   });
